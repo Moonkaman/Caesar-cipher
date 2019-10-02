f82fad7fa4cddf6c5340e617f5f5971c94f17745
Moonkaman <nickbatmanglidj@gmail.com>
2019-10-02 14:35:46 -0600

    Changes to how app functions and ease of use changes

--- caesar-cipher/src/App.js
+++ caesar-cipher/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -7,6 +7,21 @@ const App = props => {
   const [selectVal, setSelectVal] = useState("left");
   const [message, setMessage] = useState("");
   const [shift, setShift] = useState(3);
+  const [newMessage, setNewMessage] = useState("");
+
+  useEffect(
+    _ => {
+      encrypt();
+    },
+    [message, selectVal]
+  );
+
+  useEffect(
+    _ => {
+      setMessage(newMessage);
+    },
+    [selectVal]
+  );
 
   const encrypt = _ => {
     const encryptedMessage = [];
@@ -33,15 +48,15 @@ const App = props => {
         }
       }
     }
-    console.log(encryptedMessage.join(""));
+    setNewMessage(encryptedMessage.join(""));
   };
 
   return (
     <div className="App">
       <h1>Caesar Cipher</h1>
       <select value={selectVal} onChange={e => setSelectVal(e.target.value)}>
-        <option value="left">Left</option>
-        <option value="right">Right</option>
+        <option value="left">Encrypt</option>
+        <option value="right">Decrypt</option>
       </select>
       <input
         type="number"
@@ -53,7 +68,10 @@ const App = props => {
         value={message}
         onChange={e => setMessage(e.target.value.toUpperCase())}
       />
-      <button onClick={encrypt}>Encrypt Message</button>
+      {/* <button onClick={encrypt}>Submit</button> */}
+      <h3>
+        {selectVal === "left" ? "Encrypted" : "Decrypted"} message: {newMessage}
+      </h3>
     </div>
   );
 };
